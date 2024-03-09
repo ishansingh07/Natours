f2defac2c731df0946304a9fe6110faf4fe42096
irixyu <ishansingh0307@gmail.com>
2024-03-09 18:59:48 +0530

    fixed mapbox

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -12370,7 +12370,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "41495" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "45465" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
